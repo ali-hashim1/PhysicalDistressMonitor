c1a2478c9a98ccf2f99ebece90734aa30a9a5452
Ali <144294071+ali-hashim1@users.noreply.github.com>
2023-12-08 16:28:42 -0800

    Update initializing.py

--- initializing.py
+++ initializing.py
@@ -6,25 +6,27 @@ import numpy as np
 csv_file = 'Heart Rate Data-manual (4).csv'
 df = pd.read_csv(csv_file)
 
-df['Height'] = df['Height'] /272 #max human height
+df['Weight'] = df['Weight'] / 300 # avg max human weight for our user group
+df['Height'] = df['Height'] /272 #max human height in cm
 df['Age'] = df['Age'] /100 # average max age
 df['Pulse'] = df['Pulse'] /300 #maximum human pulse
 df['Temp'] = df['Temp'] /116 #maximum human temperature
 df['Oxygen'] = df['Oxygen'] /100 #maximum human oxygen
+df['Hydration'] = df['Hydration'] /512 #maximum hydration index
 
 X = df[df.columns[:-1]].values
 Y = df[df.columns[-1]].values
 
-test_data1 = np.array([[1,0,0.69,0.21,0.0,0.84,0.99]])
+test_data1 = np.array([[1,0,0.66,0.69,0.21,0.0,0.84,0.99,0.78]])
 test_labels1 = np.array([[0]])
 
-test_data2 = np.array([[1,0,0.69,0.21,0.5,0.0,0.99]])
+test_data2 = np.array([[[1,0,0.66,0.69,0.21,0.5,0.0,0.99,0.78]]])
 test_labels2 = np.array([[0]])
 
-test_data3 = np.array([[1,0,0.69,0.21,0.5,0.84,0.0]])
+test_data3 = np.array([[[1,0,0.66,0.69,0.21,0.5,0.84,0.0,0.78]]])
 test_labels3 = np.array([[0]])
 
-test_data4 = np.array([[1,0,0.69,0.21,0.5,0.84,0.99]])
+test_data4 = np.array([[[1,0,0.66,0.69,0.21,0.5,0.84,0.99,0.78]]])
 test_labels4 = np.array([[1]])
 
 
